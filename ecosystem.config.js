c3505a13dfd22a6d119c1f44290601390e2a5486
Cahyo Permono <cahyo.adi@imaniprima.com>
2025-06-16 05:29:16 +0700

    fix: update script path in ecosystem.config.js and add build script to package.json

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,18 +2,16 @@ module.exports = {
   apps: [
     {
       name: "bagsbe-app",
-      script: "src/index.ts",
+      script: "dist/index.js", // jalankan hasil build js
       instances: 1,
-      exec_mode: "fork", // ganti ke mode fork
+      exec_mode: "fork",
       autorestart: true,
       watch: false,
       max_memory_restart: "512M",
       env: {
         NODE_ENV: "production"
-      },
-      // Jika Anda menggunakan ts-node, gunakan interpreter berikut:
-      interpreter: "./node_modules/.bin/ts-node",
-      interpreter_args: "-r tsconfig-paths/register"
+      }
+      // interpreter dan interpreter_args dihapus karena sudah JS
     }
   ]
 };
