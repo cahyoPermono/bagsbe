f981d545f91fbfe58c523800d8407f139dc602f7
Cahyo Permono <cahyo.adi@imaniprima.com>
2025-06-15 20:26:16 +0700

    feat: add PM2 configuration for application management and update package.json scripts

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: "bagsbe-app",
+      script: "src/index.ts",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "512M",
+      env: {
+        NODE_ENV: "production"
+      },
+      // Jika Anda menggunakan ts-node, gunakan interpreter berikut:
+      interpreter: "./node_modules/.bin/ts-node",
+      interpreter_args: "-r tsconfig-paths/register"
+    }
+  ]
+};
